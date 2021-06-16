6b52e02fc81834795320359fc97b3822d32ef4b7
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-06-16 11:18:45 +0300

    Fix SSH login when both keyPath and ssh-agent defined (#30)
    * Fix SSH login when both keyPath and ssh-agent defined
    
    * Flip order to try the defined key first

--- ssh.go
+++ ssh.go
@@ -23,6 +23,7 @@ import (
 	"github.com/acarl005/stripansi"
 	"github.com/alessio/shellescape"
 	"github.com/k0sproject/rig/exec"
+	"github.com/k0sproject/rig/log"
 	ps "github.com/k0sproject/rig/powershell"
 
 	"github.com/mitchellh/go-homedir"
@@ -127,15 +128,7 @@ func (c *SSH) Connect() error {
 		config.HostKeyCallback = trustedHostKeyCallback(c.HostKey)
 	}
 
-	sshAgentSock := os.Getenv("SSH_AUTH_SOCK")
-
-	if sshAgentSock != "" {
-		sshAgent, err := net.Dial("unix", sshAgentSock)
-		if err != nil {
-			return fmt.Errorf("cannot connect to SSH agent auth socket %s: %s", sshAgentSock, err)
-		}
-		config.Auth = append(config.Auth, ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers))
-	}
+	var pubkeySigners []ssh.Signer
 
 	_, err := os.Stat(c.KeyPath)
 	if err != nil && !c.keypathDefault {
@@ -148,10 +141,28 @@ func (c *SSH) Connect() error {
 			return err
 		}
 		signer, err := ssh.ParsePrivateKey(key)
-		if err != nil && sshAgentSock == "" {
-			return err
+		if err != nil {
+			log.Errorf("can't parse keyfile %s: %s", c.KeyPath, err.Error())
+		} else {
+			pubkeySigners = append(pubkeySigners, signer)
 		}
-		config.Auth = append(config.Auth, ssh.PublicKeys(signer))
+	}
+
+	sshAgentSock := os.Getenv("SSH_AUTH_SOCK")
+	if sshAgentSock != "" {
+		sshAgent, err := net.Dial("unix", sshAgentSock)
+		if err != nil {
+			log.Errorf("can't connect to SSH agent auth socket %s: %s", sshAgentSock, err)
+		} else {
+			signers, err := agent.NewClient(sshAgent).Signers()
+			if err == nil {
+				pubkeySigners = append(pubkeySigners, signers...)
+			}
+		}
+	}
+
+	if len(pubkeySigners) > 0 {
+		config.Auth = append(config.Auth, ssh.PublicKeys(pubkeySigners...))
 	}
 
 	dst := fmt.Sprintf("%s:%d", c.Address, c.Port)
