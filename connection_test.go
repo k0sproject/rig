869724deb874afe7045ea7923d7c8d158da59123
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-08-18 16:23:48 +0300

    Fix sprintf templating and exec.Option grouping (#38)

--- connection_test.go
+++ connection_test.go
@@ -13,6 +13,35 @@ type Host struct {
 	Connection
 }
 
+type mockClient struct {
+	commands []string
+}
+
+func (m *mockClient) Connect() error                             { return nil }
+func (m *mockClient) Disconnect()                                {}
+func (m *mockClient) Upload(_, _ string, _ ...exec.Option) error { return nil }
+func (m *mockClient) IsWindows() bool                            { return false }
+func (m *mockClient) ExecInteractive(_ string) error             { return nil }
+func (m *mockClient) String() string                             { return "mockclient" }
+func (m *mockClient) Protocol() string                           { return "null" }
+func (m *mockClient) IPAddress() string                          { return "127.0.0.1" }
+func (m *mockClient) IsConnected() bool                          { return true }
+func (m *mockClient) Exec(cmd string, opts ...exec.Option) error {
+	o := exec.Build(opts...)
+	cmd, err := o.Command(cmd)
+	if err != nil {
+		return err
+	}
+
+	m.commands = append(m.commands, cmd)
+
+	return nil
+}
+
+var stubSudofunc = func(in string) string {
+	return "sudo-goes-here " + in
+}
+
 func TestHostFunctions(t *testing.T) {
 	h := Host{
 		Connection: Connection{
@@ -57,4 +86,31 @@ func TestOutputWriter(t *testing.T) {
 	writer := bytes.NewBuffer(buf)
 	require.NoError(t, h.Exec("echo hello world", exec.Writer(writer)))
 	require.Equal(t, "hello world\n", writer.String())
+}
+
+func TestGrouping(t *testing.T) {
+	mc := mockClient{}
+	h := Host{
+		Connection: Connection{
+			client:   &mc,
+			sudofunc: stubSudofunc,
+		},
+	}
+
+	opts, args := GroupParams(h, "ls", 1, exec.HideOutput(), exec.Sudo(h))
+	require.Len(t, opts, 2)
+	require.Len(t, args, 3)
+}
+
+func TestSudo(t *testing.T) {
+	mc := mockClient{}
+	h := Host{
+		Connection: Connection{
+			client:   &mc,
+			sudofunc: stubSudofunc,
+		},
+	}
+
+	require.NoError(t, h.Execf("ls %s", "/tmp", exec.Sudo(h)))
+	require.Contains(t, mc.commands, "sudo-goes-here ls /tmp")
 }
--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d
 	github.com/alessio/shellescape v1.4.1
 	github.com/creasty/defaults v1.5.1
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/masterzen/winrm v0.0.0-20201030141608-56ca5c5f2380
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/sirupsen/logrus v1.8.1
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 )
