4d83452ddc6c6042319390f0afbf79878e3e176e
Jim Carroll <pcarroll@mirantis.com>
2022-06-06 01:55:24 -0500

    Fix sudo usage with SLES (#61)
    * Fix sudo usage with SLES
    
    * Cleanup
    
    Signed-off-by: Kimmo Lehto <kimmo.lehto@gmail.com>
    
    * Cleanup 2
    
    Signed-off-by: Kimmo Lehto <kimmo.lehto@gmail.com>
    
    Co-authored-by: Kimmo Lehto <kimmo.lehto@gmail.com>

--- os/linux/sles.go
+++ os/linux/sles.go
@@ -1,10 +1,10 @@
 package linux
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/k0sproject/rig"
+	"github.com/k0sproject/rig/exec"
 	"github.com/k0sproject/rig/os"
 	"github.com/k0sproject/rig/os/registry"
 )
@@ -16,11 +16,10 @@ type SLES struct {
 
 // InstallPackage installs packages via zypper
 func (c SLES) InstallPackage(h os.Host, s ...string) error {
-	cmd, err := h.Sudo(fmt.Sprintf("zypper -n install -y %s", strings.Join(s, " ")))
-	if err != nil {
+	if err := h.Exec("zypper refresh", exec.Sudo(h)); err != nil {
 		return err
 	}
-	return h.Execf("zypper refresh && %s", cmd)
+	return h.Execf("zypper -n install -y %s", strings.Join(s, " "), exec.Sudo(h))
 }
 
 func init() {
