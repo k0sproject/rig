12550c1b92142f09dc310254b79dc831815a96f2
Kimmo Lehto <klehto@mirantis.com>
2023-02-07 09:47:06 +0200

    Missing file
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

---
+++ os/linux/debian.go
@@ -0,0 +1,38 @@
+// Package linux contains configurers for various linux based distributions
+package linux
+
+import (
+	"strings"
+
+	"github.com/k0sproject/rig"
+	"github.com/k0sproject/rig/exec"
+	"github.com/k0sproject/rig/os"
+	"github.com/k0sproject/rig/os/registry"
+)
+
+// Ubuntu provides OS support for Ubuntu systems
+type Debian struct {
+	os.Linux
+}
+
+func init() {
+	registry.RegisterOSModule(
+		func(os rig.OSVersion) bool {
+			return os.ID == "debian"
+		},
+		func() interface{} {
+			return Debian{}
+		},
+	)
+}
+
+// InstallPackage installs packages via apt-get
+func (c Debian) InstallPackage(h os.Host, s ...string) error {
+	if err := h.Execf("apt-get update", exec.Sudo(h)); err != nil {
+		return exec.ErrRemote.Wrapf("failed to update apt cache: %w", err)
+	}
+	if err := h.Execf("DEBIAN_FRONTEND=noninteractive apt-get install -y -q %s", strings.Join(s, " "), exec.Sudo(h)); err != nil {
+		return exec.ErrRemote.Wrapf("failed to install packages: %w", err)
+	}
+	return nil
+}
