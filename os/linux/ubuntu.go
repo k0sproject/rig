889cdc29cbce6be0035142d502b5724ed64b4160
Kimmo Lehto <klehto@mirantis.com>
2023-02-07 12:26:23 +0200

    Upgrade workflow actions (#90)
    * Upgrade workflow actions
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- os/linux/ubuntu.go
+++ os/linux/ubuntu.go
@@ -2,17 +2,13 @@
 package linux
 
 import (
-	"strings"
-
 	"github.com/k0sproject/rig"
-	"github.com/k0sproject/rig/exec"
-	"github.com/k0sproject/rig/os"
 	"github.com/k0sproject/rig/os/registry"
 )
 
 // Ubuntu provides OS support for Ubuntu systems
 type Ubuntu struct {
-	os.Linux
+	Debian
 }
 
 func init() {
@@ -24,15 +20,4 @@ func init() {
 			return Ubuntu{}
 		},
 	)
-}
-
-// InstallPackage installs packages via apt-get
-func (c Ubuntu) InstallPackage(h os.Host, s ...string) error {
-	if err := h.Execf("apt-get update", exec.Sudo(h)); err != nil {
-		return exec.ErrRemote.Wrapf("failed to update apt cache: %w", err)
-	}
-	if err := h.Execf("DEBIAN_FRONTEND=noninteractive apt-get install -y -q %s", strings.Join(s, " "), exec.Sudo(h)); err != nil {
-		return exec.ErrRemote.Wrapf("failed to install packages: %w", err)
-	}
-	return nil
 }
