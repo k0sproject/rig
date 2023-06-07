76770abd80ab7195b2c43b631102d0cffe1ae9c5
Kimmo Lehto <klehto@mirantis.com>
2023-06-07 14:02:53 +0300

    wip

--- osrelease/linuxresolver.go
+++ osrelease/linuxresolver.go
@@ -1,6 +1,13 @@
 package osrelease
 
+import "fmt"
+
 type LinuxResolver struct{}
+
+const (
+	fileCmd       = `cat /etc/os-release || cat /usr/lib/os-release`
+	lsbreleaseCmd = `command -v lsb_release 2>&1 > /dev/null && printf "NAME=%s\nID=%s\nVERSION_ID=%s\n" "$(lsb_release -sd | cut -d' ' -f1)" "$(lsb_release -si | tr '[:upper:]' '[:lower:]')" "$(lsb_release -sr)"`
+)
 
 func (g *LinuxResolver) Get(r runner) (*OSRelease, error) {
 	if r.IsWindows() {
@@ -10,9 +17,17 @@ func (g *LinuxResolver) Get(r runner) (*OSRelease, error) {
 		return nil, errNoMatch
 	}
 
-	output, err := r.Run(`cat /etc/os-release || cat /usr/lib/os-release || (command -v lsb_release 2>&1 > /dev/null && printf "NAME=%s\nID=%s\nVERSION_ID=%s\n" "$(lsb_release -sd | cut -d' ' -f1)" "$(lsb_release -si | tr '[:upper:]' '[:lower:]')" "$(lsb_release -sr)")`)
+	output, err := g.tryGet(r)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to resolve linux release info: %w", err)
 	}
+
 	return DecodeString(output)
+}
+
+func (g *LinuxResolver) tryGet(r runner) (string, error) {
+	if output, err := r.Run(fileCmd); err == nil {
+		return output, nil
+	}
+	return r.Run(lsbreleaseCmd)
 }
