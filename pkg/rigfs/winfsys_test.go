25391db8d30fff1765a14a6b2d43ae317e7f3dbc
Kimmo Lehto <klehto@mirantis.com>
2023-06-21 14:25:34 +0300

    Fix windows fsys path formatting
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

---
+++ pkg/rigfs/winfsys_test.go
@@ -0,0 +1,18 @@
+package rigfs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFormatPath(t *testing.T) {
+	fsys := NewWindowsFsys(nil)
+	require.Equal(t, "C:\\Users\\Public\\Documents\\foo.txt", fsys.formatPath("C:\\Users\\Public", "Documents\\foo.txt"))
+	require.Equal(t, "C:\\Users\\Public\\Documents\\foo.txt", fsys.formatPath("C:\\Users\\Public\\Documents\\foo.txt"))
+	require.Equal(t, "Users\\Public\\Documents\\foo.txt", fsys.formatPath("Users\\Public\\Documents\\foo.txt"))
+	require.Equal(t, "Users\\Public\\Documents\\foo.txt", fsys.formatPath("Users\\Public", "Documents\\foo.txt"))
+
+	require.Equal(t, "\\Users\\Public\\Documents\\foo.txt", fsys.formatPath("/Users/Public/Documents/foo.txt"))
+	require.Equal(t, "Users\\Public\\Documents\\foo.txt", fsys.formatPath("Users/Public", "Documents/foo.txt"))
+}
