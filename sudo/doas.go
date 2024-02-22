470303eab878c6833f8f77d6bf15f5d48da2122e
Kimmo Lehto <klehto@mirantis.com>
2024-02-22 16:22:59 +0200

    Files sudo.go and doas.go had opposite contents
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- sudo/doas.go
+++ sudo/doas.go
@@ -5,20 +5,20 @@ import (
 	"github.com/k0sproject/rig/sh/shellescape"
 )
 
-// Sudo is a DecorateFunc that will wrap the given command in a sudo call.
-func Sudo(cmd string) string {
-	return `sudo -n -- "${SHELL-sh}" -c ` + shellescape.Quote(cmd)
+// Doas is a DecorateFunc that will wrap the given command in a doas call.
+func Doas(cmd string) string {
+	return `doas -n -- "${SHELL-sh}" -c ` + shellescape.Quote(cmd)
 }
 
-// RegisterSudo registers a sudo DecorateFunc with the given repository.
-func RegisterSudo(repository *Provider) {
+// RegisterDoas registers a doas DecorateFunc with the given repository.
+func RegisterDoas(repository *Provider) {
 	repository.Register(func(c exec.Runner) (exec.Runner, bool) {
 		if c.IsWindows() {
 			return nil, false
 		}
-		if c.Exec(Sudo("true")) != nil {
+		if c.Exec(Doas("true")) != nil {
 			return nil, false
 		}
-		return exec.NewHostRunner(c, Sudo), true
+		return exec.NewHostRunner(c, Doas), true
 	})
 }
--- sudo/sudo.go
+++ sudo/sudo.go
@@ -5,20 +5,20 @@ import (
 	"github.com/k0sproject/rig/sh/shellescape"
 )
 
-// Doas is a DecorateFunc that will wrap the given command in a doas call.
-func Doas(cmd string) string {
-	return `doas -n -- "${SHELL-sh}" -c ` + shellescape.Quote(cmd)
+// Sudo is a DecorateFunc that will wrap the given command in a sudo call.
+func Sudo(cmd string) string {
+	return `sudo -n -- "${SHELL-sh}" -c ` + shellescape.Quote(cmd)
 }
 
-// RegisterDoas registers a doas DecorateFunc with the given repository.
-func RegisterDoas(repository *Provider) {
+// RegisterSudo registers a sudo DecorateFunc with the given repository.
+func RegisterSudo(repository *Provider) {
 	repository.Register(func(c exec.Runner) (exec.Runner, bool) {
 		if c.IsWindows() {
 			return nil, false
 		}
-		if c.Exec(Doas("true")) != nil {
+		if c.Exec(Sudo("true")) != nil {
 			return nil, false
 		}
-		return exec.NewHostRunner(c, Doas), true
+		return exec.NewHostRunner(c, Sudo), true
 	})
 }
