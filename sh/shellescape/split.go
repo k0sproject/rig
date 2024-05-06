147879abad0fab09e41e1d87110f5abd6067ccc5
Kimmo Lehto <klehto@mirantis.com>
2024-05-06 11:02:12 +0300

    Use go1.22, lint updates
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- sh/shellescape/split.go
+++ sh/shellescape/split.go
@@ -18,7 +18,7 @@ func Split(input string) ([]string, error) { //nolint:cyclop
 
 	var inDoubleQuotes, inSingleQuotes, isEscaped bool
 
-	for i := 0; i < len(input); i++ {
+	for i := range len(input) {
 		currentChar := input[i]
 
 		if isEscaped {
